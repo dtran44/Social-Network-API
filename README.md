b3919c67d02b7cdf3f199cd786cc1ee423ac9a21
Diana Tran <diana.tran44@gmail.com>
2024-04-04 00:22:35 +1100

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # Social-Network-API
+
+## Description
+
+The goal of this project was to build an API for a social network web application where users can share their thoughts, react to friends’ thoughts, and create a friend list.
+
+I learnt how to:
+
+- Create API routes to GET, POST, PUT and DELETE using Express
+- Create a a MongoDB database and ODM
+- To use Insomnia to test the API routes
+
+## Installation
+
+N/A
+
+## Usage
+
+GitHub Link: https://github.com/dtran44/Social-Network-API
+
+Screenshot:
+
+## License
+
+N/A
+
+## References
+
+N/A
--- utils/seed.js
+++ utils/seed.js
@@ -1,27 +1,41 @@
-const connection = require('../config/connection');
-const { User, Thought, Reaction } = require('../models');
-const { Types: { ObjectId } } = require('mongoose');
-const { getRandomName, getRandomThoughts, getRandomEmail, getRandomFriends, getRandomReactions } = require('./data');
+const connection = require("../config/connection");
+const { User, Thought, Reaction } = require("../models");
+const {
+  Types: { ObjectId },
+} = require("mongoose");
+const {
+  getRandomName,
+  getRandomThoughts,
+  getRandomEmail,
+  getRandomFriends,
+  getRandomReactions,
+} = require("./data");
 
-connection.on('error', (err) => err);
+connection.on("error", (err) => err);
 
-connection.once('open', async () => {
-  console.log('connected');
-  
+connection.once("open", async () => {
+  console.log("connected");
+
   // Delete the collections if they exist
-  let userCheck = await connection.db.listCollections({ name: 'users' }).toArray();
+  let userCheck = await connection.db
+    .listCollections({ name: "users" })
+    .toArray();
   if (userCheck.length) {
-    await connection.dropCollection('users');
+    await connection.dropCollection("users");
   }
 
-  let thoughtCheck = await connection.db.listCollections({ name: 'thoughts' }).toArray();
+  let thoughtCheck = await connection.db
+    .listCollections({ name: "thoughts" })
+    .toArray();
   if (thoughtCheck.length) {
-    await connection.dropCollection('thoughts');
+    await connection.dropCollection("thoughts");
   }
 
-  let reactionCheck = await connection.db.listCollections({ name: 'reactions' }).toArray();
+  let reactionCheck = await connection.db
+    .listCollections({ name: "reactions" })
+    .toArray();
   if (reactionCheck.length) {
-    await connection.dropCollection('reactions');
+    await connection.dropCollection("reactions");
   }
 
   // Create empty array to hold the users
@@ -33,44 +47,46 @@ connection.once('open', async () => {
     const friend = getRandomFriends().map(() => new ObjectId()); // Generate random ObjectIDs for friends using new ObjectId()
 
     const fullName = getRandomName();
-    const first = fullName.split(' ')[0];
-    const username = `${first}${Math.floor(Math.random() * (99 - 18 + 1) + 18)}`;
+    const first = fullName.split(" ")[0];
+    const username = `${first}${Math.floor(
+      Math.random() * (99 - 18 + 1) + 18
+    )}`;
 
     users.push({
       username,
       email,
-      friend
+      friend,
     });
   }
 
   // Add users to the collection and await the results
   const userData = await User.insertMany(users);
 
-  // Create empty array to hold the thoughts, add thoughts to array & add to collection
-  const thoughts = [];
-  for (let i = 0; i < 20; i++) {
-    const randomThoughts = getRandomThoughts(); 
-    thoughts.push({
-      thoughtText: randomThoughts 
-    });
-  }
-  const thoughtData = await Thought.insertMany(thoughts);
+  //   // Create empty array to hold the thoughts, add thoughts to array & add to collection
+  //   const thoughts = [];
+  //   for (let i = 0; i < 20; i++) {
+  //     const randomThoughts = getRandomThoughts();
+  //     thoughts.push({
+  //       thoughtText: randomThoughts
+  //     });
+  //   }
+  //   const thoughtData = await Thought.insertMany(thoughts);
 
- // Create empty array to hold the reactions, add reactions to array & add to collection
- const reactions = [];
- for (let i = 0; i < 20; i++) {
-   const randomReactions = getRandomReactions(); // Generate random reactions
-  
-   reactions.push({
-    randomReactions
-   });
- }
- const reactionsData = await Reaction.insertMany(reactions);
+  //  // Create empty array to hold the reactions, add reactions to array & add to collection
+  //  const reactions = [];
+  //  for (let i = 0; i < 20; i++) {
+  //    const randomReactions = getRandomReactions(); // Generate random reactions
+
+  //    reactions.push({
+  //     randomReactions
+  //    });
+  // //  }
+  //  const reactionsData = await Reaction.insertMany(reactions);
 
   // Log out the seed data to indicate what should appear in the database
   console.table(users);
-  console.table(thoughts);
-  console.table(reactions);
-  console.info('Seeding complete! 🌱');
+  // console.table(thoughts);
+  // console.table(reactions);
+  console.info("Seeding complete! 🌱");
   process.exit(0);
 });
